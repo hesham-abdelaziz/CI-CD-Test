fea58bd6c21f300f917d5a6c38fb5610188265a9
Hesham Abdalaziz <H.Abdelaziz@almaviva.it>
2026-09-21 14:25:16 +0300

    test: update signup form test case in app.spec.ts

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -14,10 +14,10 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', async () => {
+  it('should render the signup form', async () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, new-angular');
+    expect(compiled.querySelector('app-signup-form form')).toBeTruthy();
   });
 });
